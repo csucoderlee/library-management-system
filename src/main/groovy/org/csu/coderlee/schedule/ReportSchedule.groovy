df06d6534a0413af318f9ad91f75d1f6b9d9bacb
lixiang <csucoderlee@foxmail.com>
2018-05-20 22:58:16 +0800

    添加定时任务

--- src/main/groovy/org/csu/coderlee/schedule/ReportSchedule.groovy
+++ src/main/groovy/org/csu/coderlee/schedule/ReportSchedule.groovy
@@ -1,9 +1,19 @@
 package org.csu.coderlee.schedule
 
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+import java.text.SimpleDateFormat
+
 /**
  * @author by bixi.lx
  * @created on 2018 05 19 21:55
  */
+@Component
 class ReportSchedule {
 
+    @Scheduled(cron = "0/5 * * * * ?")
+    void testSchedule() {
+        println("每5s执行一次" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()))
+    }
 }
