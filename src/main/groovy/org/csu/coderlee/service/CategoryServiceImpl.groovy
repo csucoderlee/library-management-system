1da1911245a08eb2390dc6a2d3ce79836f8e29ef
lixiang <csucoderlee@foxmail.com>
2018-10-05 23:46:04 +0800

    spring mybatis 整合注入session的问题

--- src/main/groovy/org/csu/coderlee/service/CategoryServiceImpl.groovy
+++ src/main/groovy/org/csu/coderlee/service/CategoryServiceImpl.groovy
@@ -2,12 +2,14 @@ package org.csu.coderlee.service
 
 import org.csu.coderlee.dao.CategoryMapper
 import org.csu.coderlee.model.Category
+import org.springframework.stereotype.Service
 
 import javax.annotation.Resource
 
 /**
  * Created by csucoderlee on 2018/9/30
  */
+@Service
 class CategoryServiceImpl implements CategoryService {
 
     @Resource
